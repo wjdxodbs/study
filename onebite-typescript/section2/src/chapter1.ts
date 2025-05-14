a6d8c30555b2c6df360c38ad99875571b884e54b
wjdxodbs <wjdxodbs52@naver.com>
2025-05-15 01:38:08 +0900

    section3 - 원시타입과 리터럴타입입

---
+++ onebite-typescript/section2/src/chapter1.ts
@@ -0,0 +1,30 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = NaN;
+
+// num1 = "hello"; // 타입 오류
+
+// string
+let str1: string = "hello";
+let str2: string = `hello ${num1}`;
+
+// str1 = num1; // 타입 오류
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// 리터럴 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
