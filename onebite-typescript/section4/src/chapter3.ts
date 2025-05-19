9c4651891ae8e699f5099d8d30bb5de366685224
wjdxodbs <wjdxodbs52@naver.com>
2025-05-19 23:01:56 +0900

    section5 - 함수 오버로딩

---
+++ onebite-typescript/section4/src/chapter3.ts
@@ -0,0 +1,18 @@
+// 함수 오버로딩
+// 하나의 함수를 매개변수의 개수나 타입에 따라 다르게 처리할 수 있도록 하는 것
+
+// 버전들 -> 오버로드 시그니처
+function func(a: number): void;
+function func(a: number, b: number, c: number): void;
+
+// 실제 구현부 -> 구현 시그니처
+function func(a: number, b?: number, c?: number) {
+  if (typeof b === "number" && typeof c === "number") {
+    console.log(a + b + c);
+  } else {
+    console.log(a * 20);
+  }
+}
+
+func(1);
+func(1, 2, 3);
