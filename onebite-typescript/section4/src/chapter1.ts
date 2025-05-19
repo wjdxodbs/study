dc762ee5981693f11e3058df252c4c191b4e4fd1
wjdxodbs <wjdxodbs52@naver.com>
2025-05-19 16:10:20 +0900

    section5 - 함수 타입 표현식과 호출 시그니쳐

---
+++ onebite-typescript/section4/src/chapter1.ts
@@ -0,0 +1,17 @@
+// 함수 타입 표현식
+type Operation = (a: number, b: number) => number;
+
+const add: (a: number, b: number) => number = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const mul: Operation = (a, b) => a * b;
+const div: Operation = (a, b) => a / b;
+
+// 호출 시그니처 - 콜 시그니처
+type Operation2 = {
+  (a: number, b: number): number;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const mul2: Operation2 = (a, b) => a * b;
+const div2: Operation2 = (a, b) => a / b;
