53615005b29251d7e5a80f61e7360dc9e505c4b4
wjdxodbs <wjdxodbs52@naver.com>
2025-09-11 15:37:52 +0900

    section3 - SEO 설정하기

--- onebite-next/section2/src/pages/index.tsx
+++ onebite-next/section2/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { InferGetStaticPropsType } from "next";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 import fetchBooks from "@/lib/fetch-books";
 import BookItem from "@/components/BookItem";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
@@ -24,20 +25,34 @@ export default function Home({
   randomBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {randomBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta
+          property="og:image"
+          content="https://onebite-next.vercel.app/og-image.png"
+        />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {randomBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
