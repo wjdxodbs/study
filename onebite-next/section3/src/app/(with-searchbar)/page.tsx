cc1dd2ec4f3d911ad325229352131764d63c317f
wjdxodbs <wjdxodbs52@naver.com>
2025-09-16 16:54:57 +0900

    section6 - 라우트 세그먼트 옵션

--- onebite-next/section3/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section3/src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,13 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
 
+// export const dynamic = "force-dynamic";
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// auto : 기본값, 아무것도 강제하지 않음
+// force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// force-static : 페이지를 강제로 Static 페이지로 설정
+// error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
+
 async function AllBooks() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
     cache: "no-store",
