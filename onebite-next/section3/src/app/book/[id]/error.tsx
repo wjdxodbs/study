4d21100cc93abf14ea330a1f7f482e52857b40aa
wjdxodbs <wjdxodbs52@naver.com>
2025-09-17 11:08:59 +0900

    section7 - 에러 핸등링

---
+++ onebite-next/section3/src/app/book/[id]/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { startTransition, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>에러 발생</h2>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh(); // 현재 페이지에 필요한 서버컴포넌트들을 다시 불러옴
+            reset(); // 에러 상태를 초기화, 컴포넌트들을 다시 렌더링
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
