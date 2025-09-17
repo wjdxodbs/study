838c06a0c2e615734ebd48614120478f7f206c13
wjdxodbs <wjdxodbs52@naver.com>
2025-09-17 15:08:09 +0900

    section8 - 리뷰 재검증 구현하기

--- onebite-next/section3/src/actions/creat-review.action.ts
+++ onebite-next/section3/src/actions/creat-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export const createReviewAction = async (form: FormData) => {
   const bookId = form.get("bookId")?.toString();
   const content = form.get("content")?.toString();
@@ -21,6 +23,8 @@ export const createReviewAction = async (form: FormData) => {
       throw new Error("리뷰 작성에 실패했습니다.");
     }
     console.log(res.status);
+
+    revalidatePath(`/book/${bookId}`);
   } catch (error) {
     console.error(error);
     return;
